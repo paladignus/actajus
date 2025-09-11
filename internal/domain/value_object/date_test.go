071ef543dad0d91b05d7bb72e0788fe36442e25b
paladignus <marcelo@marcelo.eti.br>
2025-09-10 22:25:33 -0400

    test: ensure error if date is invalid

---
+++ internal/domain/value_object/date_test.go
@@ -0,0 +1,37 @@
+// Package valueobject
+package valueobject
+
+import (
+	"regexp"
+	"testing"
+)
+
+type Date string
+
+func (d Date) Value() string {
+	return string(d)
+}
+
+func (d Date) IsValid() bool {
+	re := regexp.MustCompile(`^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$`)
+	return re.MatchString(d.Value())
+}
+
+func TestDate(t *testing.T) {
+	t.Run("should return false if date is invalid", func(t *testing.T) {
+		datesInvalids := []Date{
+			"32/01/2020", // Dia inválido
+			"00/01/2020", // Dia inválido
+			"15/13/2020", // Mês inválido
+			"15/00/2020", // Mês inválido
+			"15/01/20",   // Ano inválido
+			"15-01-2020", // Formato inválido
+			"15012020",   // Formato inválido
+		}
+		for _, date := range datesInvalids {
+			if date.IsValid() {
+				t.Errorf("Expected Date to be invalid, but got valid")
+			}
+		}
+	})
+}
