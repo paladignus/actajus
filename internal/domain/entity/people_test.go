2d081006863162b41e668fb048cf5cca3ed2256b
paladignus <marcelo@marcelo.eti.br>
2025-09-19 20:29:04 -0400

    test:  ensure that it returns an error if the gender invalid

--- internal/domain/entity/people_test.go
+++ internal/domain/entity/people_test.go
@@ -95,4 +95,10 @@ func TestEntityPeople(t *testing.T) {
 			t.Errorf("Expected error for invalid father name, but got %v", err)
 		}
 	})
+	_, err = NewPeople(dto.PeopleInputDTO{FirstName: "John", LastName: "Doe", BirthDate: "28/08/1999", MotherName: "Jane", FatherName: "Jack"})
+	t.Run("should return error if gender is invalid", func(t *testing.T) {
+		if err == nil || !errors.Is(err, ErrInvalidGender) {
+			t.Errorf("Expected error for invalid gender, but got %v", err)
+		}
+	})
 }
