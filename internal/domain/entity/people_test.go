f8671cb44d979886a6b29afb19ded41ce7ed5ccd
paladignus <marcelo@marcelo.eti.br>
2025-09-19 20:17:12 -0400

    test:  ensure that it returns an error if the first name invalid

--- internal/domain/entity/people_test.go
+++ internal/domain/entity/people_test.go
@@ -2,50 +2,73 @@
 package entity
 
 import (
+	"errors"
+	"strings"
 	"testing"
 
 	"github.com/paladignus/juridico/internal/application/dto"
+	vo "github.com/paladignus/juridico/internal/domain/value_object"
+)
+
+var (
+	ErrInvalidFirstName = errors.New("first name is invalid")
+	ErrInvalidLastName  = errors.New("last name is invalid")
+	ErrInvalidBirthDate = errors.New("birth date is invalid")
+	ErrInvalidMother    = errors.New("mother is invalid")
+	ErrInvalidFather    = errors.New("father is invalid")
+	ErrInvalidGender    = errors.New("gender is invalid")
+	ErrInvalidMarital   = errors.New("marital status is invalid")
 )
 
 type People struct {
-	FirstName     string
-	LastName      string
-	BirthDate     string
-	MotherName    string
-	FatherName    string
-	Gender        string
-	MaritalStatus string
+	FirstName     vo.Text
+	LastName      vo.Text
+	BirthDate     vo.Date
+	Mother        vo.Text
+	Father        vo.Text
+	Gender        vo.Text
+	MaritalStatus vo.Text
 }
 
-func NewPeople(people dto.PeopleInputDTO) People {
-	return People{
-		FirstName:     people.FirstName,
-		LastName:      people.LastName,
-		BirthDate:     people.BirthDate,
-		MotherName:    people.MotherName,
-		FatherName:    people.FatherName,
-		Gender:        people.Gender,
-		MaritalStatus: people.MaritalStatus,
+func NewPeople(people dto.PeopleInputDTO) (People, error) {
+	p := People{
+		FirstName:     vo.Text(strings.TrimSpace(people.FirstName)),
+		LastName:      vo.Text(strings.TrimSpace(people.LastName)),
+		BirthDate:     vo.Date(strings.TrimSpace(people.BirthDate)),
+		Mother:        vo.Text(strings.TrimSpace(people.MotherName)),
+		Father:        vo.Text(strings.TrimSpace(people.FatherName)),
+		Gender:        vo.Text(strings.TrimSpace(people.Gender)),
+		MaritalStatus: vo.Text(strings.TrimSpace(people.MaritalStatus)),
 	}
-}
-
-func (p People) IsValid() error {
+	if !p.FirstName.IsValid() {
+		return p, ErrInvalidFirstName
+	}
+	if !p.LastName.IsValid() {
+		return p, ErrInvalidLastName
+	}
+	if !p.BirthDate.IsValid() {
+		return p, ErrInvalidBirthDate
+	}
+	if !p.Mother.IsValid() {
+		return p, ErrInvalidMother
+	}
+	if !p.Father.IsValid() {
+		return p, ErrInvalidFather
+	}
+	if !p.Gender.IsValid() {
+		return p, ErrInvalidGender
+	}
+	if !p.MaritalStatus.IsValid() {
+		return p, ErrInvalidMarital
+	}
+	return p, nil
 }
 
 func TestEntityPeople(t *testing.T) {
-	sut := NewPeople(dto.PeopleInputDTO{
-		FirstName:     "John",
-		LastName:      "Doe",
-		BirthDate:     "1980-02-30",
-		MotherName:    "Helena Doe",
-		FatherName:    "Martin Doe",
-		Gender:        "Masculino",
-		MaritalStatus: "Solteiro",
-	})
-	t.Run("should return error if FirstName is empty", func(t *testing.T) {
-		sut.FirstName = ""
-		if err := sut.IsValid(); err == nil {
-			t.Errorf("Expected error for empty name, but got nil")
+	_, err := NewPeople(dto.PeopleInputDTO{})
+	t.Run("should return error if first name is invalid", func(t *testing.T) {
+		if err == nil || !errors.Is(err, ErrInvalidFirstName) {
+			t.Errorf("Expected error for invalid first name, but got %v", err)
 		}
 	})
 }
