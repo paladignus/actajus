30752a70fed4963ab5a7facf53f4b464dd23d678
paladignus <marcelo@marcelo.eti.br>
2025-09-22 15:58:45 -0400

    test: use case unique call repository

--- internal/application/usecase/signin_test.go
+++ internal/application/usecase/signin_test.go
@@ -1,7 +1,6 @@
 package usecase
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -19,8 +18,7 @@ func (a *AuthenticateMockPersistence) Authenticate(username, password string) (s
 	a.Username = username
 	a.Password = password
 	a.callsCount++
-	fmt.Println("Authenticate called", a.callsCount, "times")
-	return "id_user", nil
+	return NewRandomString(4), nil
 }
 
 type SignIn struct {
@@ -41,14 +39,30 @@ func TestSignIn(t *testing.T) {
 	repository := AuthenticateMockPersistence{}
 	sut := NewSignIn(&repository)
 	sut.Execute(username, password)
+
 	t.Run("should corrects properties from repository", func(t *testing.T) {
 		if repository.Username != username || repository.Password != password {
 			t.Error("Expected username and password to match")
 		}
 	})
+
 	t.Run("should call repository only once", func(t *testing.T) {
 		if repository.callsCount != 1 {
 			t.Error("Expected callsCount to be 1")
+		}
+	})
+
+	t.Run("should return user id", func(t *testing.T) {
+		userID, _ := sut.Execute(username, password)
+		if userID == "" {
+			t.Error("Expected userID to be not empty")
+		}
+	})
+
+	t.Run("should return an error", func(t *testing.T) {
+		_, err := sut.Execute(username, password)
+		if err == nil {
+			t.Error("Expected error to be nil")
 		}
 	})
 }
