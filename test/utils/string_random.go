5b2db23bfa1d5d1604ba46a55503afa3eb6c4087
paladignus <marcelo@marcelo.eti.br>
2025-09-25 12:50:15 -0400

    feat: added function to random strings

---
+++ test/utils/string_random.go
@@ -0,0 +1,11 @@
+// Package utils
+package utils
+
+func NewRandomString(n int) string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[i%len(letters)]
+	}
+	return string(b)
+}
