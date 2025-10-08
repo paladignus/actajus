d633354503868449534588e2a1423111406499c3
paladignus <marcelo@marcelo.eti.br>
2025-10-08 16:03:05 -0400

    fix: added query to email

--- internal/infrastructure/persistence/authenticate.go
+++ internal/infrastructure/persistence/authenticate.go
@@ -22,12 +22,13 @@ func NewAuthenticate(db *postgres.DB) Authenticate {
 func (a Authenticate) SignIn(ctx context.Context, cpf string, password string) (dto.AuthenticatedOutput, error) {
 	autenticated := dto.AuthenticatedOutput{}
 	var idaccount string
-	sql := `SELECT DISTINCT p.idpeople, p.first_name, p.last_name, a.idaccounts FROM people p
+	sql := `SELECT p.idpeople, p.first_name, p.last_name, e.address, a.idaccounts FROM people p
 		INNER JOIN documents d ON p.idpeople = d.id_people
-		INNER JOIN accounts a ON p.idpeople = a.id_people 
+		INNER JOIN accounts a ON p.idpeople = a.id_people
+		INNER JOIN emails e ON p.idpeople = e.id_people
 		WHERE d.cpf = $1 AND a.password = crypt($2, password);`
 	err := a.db.Pool.QueryRow(ctx, sql, cpf, password).
-		Scan(&autenticated.UserID, &autenticated.FirstName, &autenticated.LastName, &idaccount)
+		Scan(&autenticated.UserID, &autenticated.FirstName, &autenticated.LastName, &autenticated.Email, &idaccount)
 	if errors.Is(err, pgx.ErrNoRows) {
 		return autenticated, domainerrors.ErrUserNotFound
 	}
