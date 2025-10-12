18340d973cf618485680dbcbcdb5258daea36292
paladignus <marcelo@marcelo.eti.br>
2025-10-11 20:34:51 -0400

    refactor: simplified into a single file

--- internal/application/dto/token.go
+++ internal/application/dto/token.go
@@ -1,6 +1,11 @@
 // Package dto
 package dto
 
+type TokenClaims struct {
+	IDUser    string `json:"id_user"`
+	TokenType string `json:"token_type"`
+}
+
 type TokenPair struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
--- internal/application/dto/token_claims.go
+++
@@ -1,7 +0,0 @@
-// Package dto
-package dto
-
-type TokenClaims struct {
-	UserID    string `json:"user_id"`
-	TokenType string `json:"token_type"`
-}
