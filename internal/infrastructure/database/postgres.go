758710f74298045e5976076ba1d6575d0c24e090
paladignus <marcelo@marcelo.eti.br>
2025-10-16 17:08:23 -0400

    feat: added interface PxgPool to tests

--- internal/infrastructure/database/postgres.go
+++ internal/infrastructure/database/postgres.go
@@ -5,27 +5,33 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/paladignus/actajus/internal/domain/repository"
 	"github.com/paladignus/actajus/internal/infrastructure/config"
 )
 
+type PgxPool interface {
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+	Exec(ctx context.Context, sql string, args ...any) (pgconn.CommandTag, error)
+	Close()
+}
+
 type DB struct {
-	Pool *pgxpool.Pool
+	Pool PgxPool
 }
 
 func NewConnection(ctx context.Context, cfg *config.DatabaseConfig, logger repository.Logger) (*DB, error) {
 	dsn := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
 		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DBName, cfg.SSLMode)
-
 	poolConfig, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
 		return nil, fmt.Errorf("unable to parse database config: %w", err)
 	}
-
 	poolConfig.MaxConns = 25
 	poolConfig.MinConns = 5
-
 	pool, err := pgxpool.NewWithConfig(ctx, poolConfig)
 	if err != nil {
 		return nil, fmt.Errorf("unable to create connection pool: %w", err)
@@ -33,7 +39,6 @@ func NewConnection(ctx context.Context, cfg *config.DatabaseConfig, logger repos
 	if err := pool.Ping(ctx); err != nil {
 		return nil, fmt.Errorf("unable to connect to database: %w", err)
 	}
-
 	logger.Info(ctx, "database connection sucessfully")
 	return &DB{Pool: pool}, nil
 }
