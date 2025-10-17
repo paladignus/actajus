025e16122467420fdbfb9349984271b4452db39a
paladignus <marcelo@marcelo.eti.br>
2025-10-17 11:12:44 -0400

    refactor: removed service and controller

--- internal/infrastructure/http/handler/authenticate.go
+++ internal/infrastructure/http/handler/authenticate.go
@@ -5,17 +5,17 @@ import (
 	"net/http"
 
 	"github.com/paladignus/actajus/internal/application/dto"
-	"github.com/paladignus/actajus/internal/application/service"
+	"github.com/paladignus/actajus/internal/application/usecase"
 	"github.com/paladignus/actajus/internal/domain/repository"
 )
 
 type Authenticate struct {
-	service service.Authenticate
+	usecase usecase.Authenticate
 	logger  repository.Logger
 }
 
-func NewAuthenticate(service service.Authenticate, logger repository.Logger) Authenticate {
-	return Authenticate{service, logger}
+func NewAuthenticate(usecase usecase.Authenticate, logger repository.Logger) Authenticate {
+	return Authenticate{usecase, logger}
 }
 
 func (a Authenticate) Authenticate(w http.ResponseWriter, r *http.Request) {
@@ -26,7 +26,7 @@ func (a Authenticate) Authenticate(w http.ResponseWriter, r *http.Request) {
 		RespondError(w, http.StatusBadRequest, err.Error())
 		return
 	}
-	resp, err := a.service.Authenticate(r.Context(), req)
+	resp, err := a.usecase.Execute(r.Context(), req)
 	if err != nil {
 		statusCode, errResponse := MapDomainErrorToHTTP(err)
 		if statusCode >= 500 {
@@ -39,41 +39,4 @@ func (a Authenticate) Authenticate(w http.ResponseWriter, r *http.Request) {
 	}
 	a.logger.Info(r.Context(), "authentication successful", "cpf", req.CPF)
 	RespondJSON(w, http.StatusOK, resp)
-	// a.logger.Info(r.Context(), "processing sign_in request")
-	// var req dto.AuthenticateInput
-	// if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-	// 	a.logger.Warn(r.Context(), "invalid request body",
-	// 		"error", err,
-	// 	)
-	// 	http.Error(w, err.Error(), http.StatusBadRequest)
-	// 	return
-	// }
-	// resp, err := a.service.Authenticate(r.Context(), req)
-	// if err != nil {
-	// 	statusCode, errResponse := MapDomainErrorToHTTP(err)
-	// 	if statusCode >= 500 {
-	// 		a.logger.Error(r.Context(), "authentication failed with server error",
-	// 			"error", err,
-	// 			"cpf", req.CPF,
-	// 		)
-	// 	} else {
-	// 		a.logger.Warn(r.Context(), "authentication failed",
-	// 			"error", err,
-	// 			"cpf", req.CPF,
-	// 			"status_code", statusCode,
-	// 		)
-	// 	}
-	// 	w.Header().Set("Content-Type", "application/json")
-	// 	w.WriteHeader(statusCode)
-	// 	json.NewEncoder(w).Encode(errResponse)
-	// 	return
-	// }
-	// a.logger.Info(r.Context(), "authentication successful",
-	// 	"cpf", req.CPF,
-	// )
-	// w.Header().Set("Content-Type", "application/json")
-	// if err := json.NewEncoder(w).Encode(resp); err != nil {
-	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-	// 	return
-	// }
 }
