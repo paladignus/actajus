cffcab838c2a6d5a766d5f273c63562c5b73b24c
paladignus <marcelo@marcelo.eti.br>
2025-10-22 11:03:25 -0400

    test: ensure an valid access and refresh token pair

--- internal/infrastructure/adapter/jwt_test.go
+++ internal/infrastructure/adapter/jwt_test.go
@@ -162,3 +162,58 @@ func TestValidateRefreshToken(t *testing.T) {
 		})
 	}
 }
+
+func TestRefreshAccessToken(t *testing.T) {
+	cfg := getTestConfig()
+	adapter := NewJWTAdapter(cfg)
+	validTokenPair, _ := adapter.GenerateTokenPair("user-789")
+	expiredCfg := getTestConfig()
+	expiredCfg.RefreshExpiry = -1 * time.Hour
+	expiredAdapter := NewJWTAdapter(expiredCfg)
+	expiredTokenPair, _ := expiredAdapter.GenerateTokenPair("user-789")
+	tests := []struct {
+		name         string
+		refreshToken string
+		wantErr      bool
+		validateUser string
+	}{
+		{
+			name:         "should return an valid refresh token",
+			refreshToken: validTokenPair.RefreshToken,
+			validateUser: "user-789",
+		},
+		{
+			name:         "should return an error to invalid token format",
+			refreshToken: "invalid-token",
+			wantErr:      true,
+		},
+		{
+			name:         "should return an error to expired refresh token",
+			refreshToken: expiredTokenPair.RefreshToken,
+			wantErr:      true,
+		},
+		{
+			name:         "should return an error to access token instead of refresh",
+			refreshToken: validTokenPair.AccessToken,
+			wantErr:      true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			newTokenPair, err := adapter.RefreshAccessToken(tt.refreshToken)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			assert.NotEmpty(t, newTokenPair.AccessToken)
+			assert.NotEmpty(t, newTokenPair.RefreshToken)
+			accessClaims, err := adapter.ValidateAccessToken(newTokenPair.AccessToken)
+			require.NoError(t, err)
+			assert.Equal(t, tt.validateUser, accessClaims.IDUser)
+			refreshClaims, err := adapter.ValidateRefreshToken(newTokenPair.RefreshToken)
+			require.NoError(t, err)
+			assert.Equal(t, tt.validateUser, refreshClaims.IDUser)
+		})
+	}
+}
