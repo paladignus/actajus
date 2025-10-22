b6090cd73d434a56d4f1f36fd751a84b443d14e0
paladignus <marcelo@marcelo.eti.br>
2025-10-22 10:54:06 -0400

    test: ensure a valid access token

--- internal/infrastructure/adapter/jwt_test.go
+++ internal/infrastructure/adapter/jwt_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/paladignus/actajus/internal/infrastructure/config"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -26,4 +27,71 @@ func TestGenerateTokenPair(t *testing.T) {
 	assert.NotEmpty(t, tokenPair.AccessToken)
 	assert.NotEmpty(t, tokenPair.RefreshToken)
 	assert.NotEqual(t, tokenPair.AccessToken, tokenPair.RefreshToken)
+}
+
+func TestValidateAccessToken(t *testing.T) {
+	cfg := getTestConfig()
+	adapter := NewJWTAdapter(cfg)
+	validTokenPair, _ := adapter.GenerateTokenPair("user-123")
+	expiredCfg := getTestConfig()
+	expiredCfg.AccessExpiry = -1 * time.Hour
+	expiredAdapter := NewJWTAdapter(expiredCfg)
+	expiredTokenPair, _ := expiredAdapter.GenerateTokenPair("user-123")
+	wrongSecretCfg := getTestConfig()
+	wrongSecretCfg.AccessSecret = "wrong-secret"
+	wrongSecretToken, _ := NewJWTAdapter(wrongSecretCfg).GenerateTokenPair("user-123")
+	invalidClaimsToken := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
+		"sub": "user-123",
+		"exp": time.Now().Add(time.Hour).Unix(),
+	})
+	invalidClaimsTokenString, _ := invalidClaimsToken.SignedString([]byte(cfg.AccessSecret))
+	tests := []struct {
+		name     string
+		token    string
+		wantErr  error
+		wantUser string
+	}{
+		{
+			name:     "should return valid access token",
+			token:    validTokenPair.AccessToken,
+			wantUser: "user-123",
+		},
+		{
+			name:    "should return invalid token error to token invalid",
+			token:   "invalid-token",
+			wantErr: ErrInvalidToken,
+		},
+		{
+			name:    "should return invalid token error to empty token",
+			token:   "",
+			wantErr: ErrInvalidToken,
+		},
+		{
+			name:    "should return expired token error",
+			token:   expiredTokenPair.AccessToken,
+			wantErr: ErrExpiredToken,
+		},
+		{
+			name:    "should return invalid token error to wrong secret token",
+			token:   wrongSecretToken.AccessToken,
+			wantErr: ErrInvalidToken,
+		},
+		{
+			name:    "should return invalid token type error",
+			token:   invalidClaimsTokenString,
+			wantErr: ErrInvalidTokenType,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			claims, err := adapter.ValidateAccessToken(tt.token)
+			if tt.wantErr != nil {
+				assert.ErrorIs(t, err, tt.wantErr)
+				return
+			}
+			require.NoError(t, err)
+			assert.Equal(t, tt.wantUser, claims.IDUser)
+			assert.Equal(t, "access", claims.TokenType)
+		})
+	}
 }
