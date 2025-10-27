ece9cdfcfd24f993109e4a430efbcb4a70dc14db
paladignus <marcelo@marcelo.eti.br>
2025-10-27 15:02:57 -0400

    fix: added dto input and output get email by cpf

--- internal/application/usecase/get_email_by_cpf.go
+++ internal/application/usecase/get_email_by_cpf.go
@@ -4,6 +4,7 @@ package usecase
 import (
 	"context"
 
+	"github.com/paladignus/actajus/internal/application/dto"
 	"github.com/paladignus/actajus/internal/domain/exception"
 	"github.com/paladignus/actajus/internal/domain/repository"
 	vo "github.com/paladignus/actajus/internal/domain/value_object"
@@ -18,18 +19,18 @@ func NewGetEmailByCPF(persistence repository.Account, logger repository.Logger) 
 	return GetEmailByCPF{persistence, logger}
 }
 
-func (g GetEmailByCPF) Execute(ctx context.Context, cpfString string) (string, error) {
-	g.logger.Info(ctx, "getting email by CPF", "cpf", cpfString)
-	cpf := vo.CPF(cpfString)
+func (g GetEmailByCPF) Execute(ctx context.Context, req dto.GetEmailByCPFInput) (dto.GetEmailByCPFOutput, error) {
+	g.logger.Info(ctx, "getting email by CPF", "cpf", req.CPF)
+	cpf := vo.CPF(req.CPF)
 	if !cpf.IsValid() {
-		g.logger.Warn(ctx, "invalid cpf format provided", "cpf", cpfString)
-		return "", exception.ErrInvalidCPF
+		g.logger.Warn(ctx, "invalid cpf format provided", "cpf", req.CPF)
+		return dto.GetEmailByCPFOutput{}, exception.ErrInvalidCPF
 	}
 	email, err := g.persistence.GetEmailByCPF(ctx, cpf.OnlyDigits())
 	if err != nil {
-		g.logger.Warn(ctx, "email not found for provided cpf", "cpf", cpfString, "error", err)
-		return "", exception.ErrEmailNotFound
+		g.logger.Warn(ctx, "email not found for provided cpf", "cpf", req.CPF, "error", err)
+		return dto.GetEmailByCPFOutput{}, exception.ErrEmailNotFound
 	}
-	g.logger.Info(ctx, "email found successfully", "cpf", cpfString, "email", email)
-	return email, nil
+	g.logger.Info(ctx, "email found successfully", "cpf", req.CPF, "email", email)
+	return dto.GetEmailByCPFOutput{Email: email}, nil
 }
--- internal/application/usecase/get_email_by_cpf_test.go
+++ internal/application/usecase/get_email_by_cpf_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/paladignus/actajus/internal/application/dto"
 	"github.com/paladignus/actajus/internal/domain/exception"
 	"github.com/paladignus/actajus/test/infrastructure/persistence/spy"
 	"github.com/stretchr/testify/assert"
@@ -14,23 +15,26 @@ func TestGetEmailByCPF(t *testing.T) {
 	ctx := context.Background()
 	account := &spy.AuthenticateSpy{}
 	logger := &spy.MockLogger{}
+	req := dto.GetEmailByCPFInput{CPF: "111.444.777-35"}
 	sut := NewGetEmailByCPF(account, logger)
 	t.Run("should return of an email when the CPF is valid and found", func(t *testing.T) {
 		account.FindResult.Email = "email@example.com.br"
-		email, err := sut.Execute(ctx, "111.444.777-35")
+		email, err := sut.Execute(ctx, req)
 		assert.NoError(t, err)
 		assert.NotEmpty(t, email)
 	})
 
 	t.Run("should return an error when the CPF is invalid", func(t *testing.T) {
-		_, err := sut.Execute(ctx, "123.456.789-00")
+		req.CPF = "123.456.789-00"
+		_, err := sut.Execute(ctx, req)
 		assert.ErrorIs(t, err, exception.ErrInvalidCPF)
 	})
 
 	t.Run("should return an error if the CPF is not found", func(t *testing.T) {
+		req.CPF = "123.456.789-09"
 		wantErr := exception.ErrEmailNotFound
 		account.FindError = wantErr
-		_, err := sut.Execute(ctx, "123.456.789-09")
+		_, err := sut.Execute(ctx, req)
 		assert.ErrorIs(t, err, wantErr)
 	})
 }
