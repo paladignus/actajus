d3dcc7e33a48791e0d88c83d714c54d69f4b7bd1
paladignus <marcelo@marcelo.eti.br>
2025-10-29 15:52:52 -0400

    fix: fixed to param input to query in database

--- internal/infrastructure/persistence/account.go
+++ internal/infrastructure/persistence/account.go
@@ -101,16 +101,17 @@ func (a Account) GetPermissionsByRoleID(ctx context.Context, idrole int) (permis
 	return permissions, nil
 }
 
-func (a Account) FindEmailByCPF(ctx context.Context, cpf dto.GetEmailByCPFInput) (email dto.GetEmailByCPFOutput, err error) {
+func (a Account) FindEmailByCPF(ctx context.Context, input dto.GetEmailByCPFInput) (output dto.GetEmailByCPFOutput, err error) {
 	sql := `SELECT e.address FROM emails e
 		INNER JOIN people p ON e.id_people = p.idpeople
+		INNER JOIN accounts a ON p.idpeople = a.id_people
 		INNER JOIN documents d ON d.id_people = p.idpeople
-		WHERE e.deleted_at IS NULL AND p.deleted_at IS NULL AND d.cpf = $1;`
-	if err = a.db.Pool.QueryRow(ctx, sql, cpf).Scan(&email.Email); err != nil {
+		WHERE a.deleted_at IS NULL AND e.deleted_at IS NULL AND p.deleted_at IS NULL AND d.cpf = $1;`
+	if err = a.db.Pool.QueryRow(ctx, sql, input.CPF).Scan(&output.Email); err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
 			return dto.GetEmailByCPFOutput{}, exception.ErrEmailNotFound
 		}
 		return dto.GetEmailByCPFOutput{}, err
 	}
-	return email, nil
+	return output, nil
 }
