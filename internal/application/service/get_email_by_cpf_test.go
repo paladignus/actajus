7848a58426ae72a9ca9ecd2f234b8e04254488b5
paladignus <marcelo@marcelo.eti.br>
2025-11-25 12:35:28 -0400

    test: ensure service get email email by cpf is valid

---
+++ internal/application/service/get_email_by_cpf_test.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/paladignus/actajus/internal/application/dto"
+	"github.com/paladignus/actajus/test/spy"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetEmailByCPFInterface(t *testing.T) {
+	ctx := context.Background()
+	mockService := &spy.MockGetEmailByCPF{
+		ExpectedOutput: dto.GetEmailByCPFOutput{
+			Email: "test@example.com",
+		},
+		ExpectedError: nil,
+	}
+	input := dto.GetEmailByCPFInput{
+		CPF: "12345678901",
+	}
+	t.Run("should return the same email", func(t *testing.T) {
+		output, err := mockService.Execute(ctx, input)
+		assert.NoError(t, err)
+		assert.Equal(t, "test@example.com", output.Email)
+	})
+	t.Run("should return an empty email", func(t *testing.T) {
+		mockService = &spy.MockGetEmailByCPF{
+			ExpectedOutput: dto.GetEmailByCPFOutput{},
+			ExpectedError:  errors.New("test error"),
+		}
+		_, err := mockService.Execute(ctx, input)
+		assert.Error(t, err)
+		assert.Equal(t, "test error", err.Error())
+	})
+}
+
