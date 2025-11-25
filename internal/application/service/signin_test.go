fe8e36a211ddb7d82228fb0e25785948018feeaa
paladignus <marcelo@marcelo.eti.br>
2025-11-25 12:47:48 -0400

    test: ensure service signin is valid

---
+++ internal/application/service/signin_test.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/paladignus/actajus/internal/application/dto"
+	"github.com/paladignus/actajus/test/spy"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSignInInterface(t *testing.T) {
+	ctx := context.Background()
+	mockService := &spy.MockSignIn{
+		ExpectedOutput: dto.SignInOutput{
+			IDUser:       "user-123",
+			FirstName:    "John",
+			LastName:     "Doe",
+			Email:        "john.doe@example.com",
+			AccessToken:  "access-token",
+			RefreshToken: "refresh-token",
+		},
+		ExpectedError: nil,
+	}
+	input := dto.SignInInput{
+		CPF:      "12345678901",
+		Password: "password123",
+	}
+	t.Run("should return the same IDUser, FirstName, Email, AccessToken and RefreshToken", func(t *testing.T) {
+		output, err := mockService.Execute(ctx, input)
+		assert.NoError(t, err)
+		assert.Equal(t, "user-123", output.IDUser)
+		assert.Equal(t, "John", output.FirstName)
+		assert.Equal(t, "john.doe@example.com", output.Email)
+	})
+
+	t.Run("should return an empty IDUser, FirstName, Email, AccessToken and RefreshToken", func(t *testing.T) {
+		mockService := &spy.MockSignIn{
+			ExpectedOutput: dto.SignInOutput{},
+			ExpectedError:  errors.New("authentication failed"),
+		}
+		_, err := mockService.Execute(ctx, input)
+		assert.Error(t, err)
+		assert.Equal(t, "authentication failed", err.Error())
+	})
+}
+
