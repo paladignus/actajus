0964f3c31a188ba1e2e205f932032314e5662780
paladignus <marcelo@marcelo.eti.br>
2025-11-25 14:06:20 -0400

    test: ensure recover password use case is valid

--- internal/application/usecase/recover_password.go
+++ internal/application/usecase/recover_password.go
@@ -29,7 +29,6 @@ func NewRecoverPassword(
 }
 
 func (r RecoverPassword) Execute(ctx context.Context, req dto.RecoverPasswordInput) error {
-	r.logger.Info(ctx, "recover password", "email", req.Email)
 	email := vo.Email(req.Email)
 	if !email.IsValid() {
 		r.logger.Warn(ctx, "invalid email format provided",
@@ -37,38 +36,32 @@ func (r RecoverPassword) Execute(ctx context.Context, req dto.RecoverPasswordInp
 		)
 		return exception.ErrInvalidEmail
 	}
-	IDUser, err := r.persistence.AccountIsActive(ctx, req.Email)
+	idUser, err := r.persistence.AccountIsActive(ctx, req.Email)
 	if err != nil {
 		r.logger.Error(ctx, "account not found or is inactive", "error", err, "email", req.Email)
 		return err
 	}
-	r.logger.Info(ctx, "account is ative to email", "email", req.Email)
-	token, err := r.token.GenerateResetToken(IDUser)
+	token, err := r.token.GenerateResetToken(idUser)
 	if err != nil {
 		r.logger.Error(ctx, "failed to generate reset token", "error", err, "email", req.Email)
 		return err
 	}
-	r.logger.Info(ctx, "invalidating previous token", "id_user", IDUser)
-	if err := r.persistence.InvalidAllTokensByIDUser(ctx, IDUser); err != nil {
-		r.logger.Error(ctx, "failed to invalidate previous token", "error", err, "id_user", IDUser)
+	if err := r.persistence.InvalidAllTokensByIDUser(ctx, idUser); err != nil {
+		r.logger.Error(ctx, "failed to invalidate previous token", "error", err, "id_user", idUser)
 		return err
 	}
-	r.logger.Info(ctx, "generated reset token", "email", req.Email)
-	if err = r.persistence.CreateRecoverPassword(ctx, IDUser, token.ResetToken); err != nil {
+	if err = r.persistence.CreateRecoverPassword(ctx, idUser, token.ResetToken); err != nil {
 		r.logger.Error(ctx, "failed to create reset token record", "error", err, "email", req.Email)
 		return err
 	}
-	evt := event.NewPasswordResetRequestedEvent(
-		IDUser,
+	if err := r.publisher.Publish(ctx, event.NewPasswordResetRequestedEvent(
+		idUser,
 		req.Email,
-		token.ResetToken,
 		"https://api.actajus.com.br/recover-password?token="+token.ResetToken,
-		"Marcelo Marretado",
-		// user.Name,
-	)
-	if err := r.publisher.Publish(ctx, evt); err != nil {
+	)); err != nil {
 		r.logger.Error(ctx, "failed to publish recover password event", "error", err, "email", req.Email)
 		return err
 	}
+	r.logger.Info(ctx, "recover password successfully", "email", req.Email)
 	return nil
 }
--- internal/application/usecase/recover_password_test.go
+++ internal/application/usecase/recover_password_test.go
@@ -18,13 +18,11 @@ func TestRecoverPassword(t *testing.T) {
 	authentication := spy.NewAuthenticationSpy()
 	logger := &spy.SpyLogger{}
 	token := &spy.SpyToken{}
-	// smtp := &spy.SpySMTP{}
 	publisher := &spy.SpyPublisher{}
 	sut := NewRecoverPassword(authentication, logger, token, publisher)
 	input := dto.RecoverPasswordInput{Email: "email"}
 
 	t.Run("should return error if email is invalid", func(t *testing.T) {
-		logger.On("Info", ctx, "recover password", "email", input.Email).Once()
 		logger.On("Warn", ctx, "invalid email format provided", "email", input.Email).Once()
 		err := sut.Execute(ctx, input)
 		assert.Error(t, err)
@@ -35,7 +33,6 @@ func TestRecoverPassword(t *testing.T) {
 		wantErr := errors.New("failed to find id user")
 		authentication.FindError = wantErr
 		input.Email = "email@example.com.br"
-		logger.On("Info", ctx, "recover password", "email", input.Email).Once()
 		logger.On("Error", ctx, "account not found or is inactive", "error", authentication.FindError, "email", input.Email).Once()
 		err := sut.Execute(ctx, input)
 		assert.Error(t, err)
@@ -46,51 +43,45 @@ func TestRecoverPassword(t *testing.T) {
 		wantErr := adapter.ErrBuildToken
 		token.Err = wantErr
 		authentication.FindError = nil
-		logger.On("Info", ctx, "recover password", "email", input.Email).Once()
-		logger.On("Info", ctx, "account is ative to email", "email", input.Email).Once()
 		logger.On("Error", ctx, "failed to generate reset token", "error", token.Err, "email", input.Email).Once()
 		err := sut.Execute(ctx, input)
 		assert.Error(t, err)
 		assert.ErrorIs(t, err, wantErr)
 	})
 
+	t.Run("should return an error if it fails to invalidate previous token", func(t *testing.T) {
+		wantErr := errors.New("failed to invalidate previous token")
+		authentication.InvalidAllTokensError = wantErr
+		token.Err = nil
+		logger.On("Error", ctx, "failed to invalidate previous token", "error", authentication.InvalidAllTokensError, "id_user", authentication.FindResult.Authentication.IDUser).Once()
+		err := sut.Execute(ctx, input)
+		assert.Error(t, err)
+		assert.ErrorIs(t, err, wantErr)
+	})
+
 	t.Run("should return an error if it fails to create recover password", func(t *testing.T) {
-		t.SkipNow()
 		wantErr := errors.New("failed to create recover password")
 		authentication.ValidateError = wantErr
-		token.Err = nil
-		logger.On("Info", ctx, "recover password", "email", input.Email).Once()
-		logger.On("Info", ctx, "account is ative to email", "email", input.Email).Once()
-		logger.On("Info", ctx, "generated reset token", "email", input.Email).Once()
+		authentication.InvalidAllTokensError = nil
 		logger.On("Error", ctx, "failed to create reset token record", "error", authentication.ValidateError, "email", input.Email).Once()
 		err := sut.Execute(ctx, input)
 		assert.Error(t, err)
 		assert.ErrorIs(t, err, wantErr)
 	})
 
-	// t.Run("should return an error if it fails to send email", func(t *testing.T) {
-	// 	wantErr := errors.New("failed to send email")
-	// 	smtp.Err = wantErr
-	// 	authentication.ValidateError = nil
-	// 	logger.On("Info", ctx, "recover password", "email", input.Email).Once()
-	// 	logger.On("Info", ctx, "account is ative to email", "email", input.Email).Once()
-	// 	logger.On("Info", ctx, "generated reset token", "email", input.Email).Once()
-	// 	logger.On("Info", ctx, "created reset token record", "email", input.Email).Once()
-	// 	logger.On("Error", ctx, "failed to send email", "error", smtp.Err, "email", input.Email).Once()
-	// 	err := sut.Execute(ctx, input)
-	// 	assert.Error(t, err)
-	// 	assert.ErrorIs(t, err, wantErr)
-	// })
+	t.Run("should return an error if it fails publisher event", func(t *testing.T) {
+		wantErr := errors.New("failed to publisher event")
+		publisher.Err = wantErr
+		authentication.ValidateError = nil
+		logger.On("Error", ctx, "failed to publish recover password event", "error", publisher.Err, "email", input.Email).Once()
+		err := sut.Execute(ctx, input)
+		assert.Error(t, err)
+		assert.ErrorIs(t, err, wantErr)
+	})
 
 	t.Run("should successful to recover password", func(t *testing.T) {
-		t.SkipNow()
-		// smtp.Err = nil
-		authentication.ValidateError = nil
-		logger.On("Info", ctx, "recover password", "email", input.Email).Once()
-		logger.On("Info", ctx, "account is ative to email", "email", input.Email).Once()
-		logger.On("Info", ctx, "generated reset token", "email", input.Email).Once()
-		// logger.On("Info", ctx, "created reset token record", "email", input.Email).Once()
-		logger.On("Info", ctx, "recover password successful", "email", input.Email)
+		publisher.Err = nil
+		logger.On("Info", ctx, "recover password successfully", "email", input.Email)
 		err := sut.Execute(ctx, input)
 		assert.NoError(t, err)
 	})
