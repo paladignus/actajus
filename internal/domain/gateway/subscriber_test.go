618ac285fb9836a638498bdd7201e2fe6be2d8fe
paladignus <marcelo@marcelo.eti.br>
2025-11-28 10:19:34 -0400

    recafctor: removed unnacessary comments

--- internal/domain/gateway/subscriber_test.go
+++ internal/domain/gateway/subscriber_test.go
@@ -7,32 +7,6 @@ import (
 	"github.com/paladignus/actajus/test/spy"
 )
 
-// MockHandler is a mock implementation of the repository.IHandler interface for testing
-
-// func (m *MockHandler) Handle(ctx context.Context, event event.IEvent) error {
-// 	return nil
-// }
-//
-// func (m *MockHandler) CanHandle(event event.IEvent) bool {
-// 	return true
-// }
-//
-// // MockSubscriber is a mock implementation of the Subscriber interface for testing
-// type MockSubscriber struct{}
-//
-// func (m *MockSubscriber) Subscribe(ctx context.Context, eventName string, handler repository.IHandler) error {
-// 	return nil
-// }
-//
-// func (m *MockSubscriber) Unsubscribe(eventName string) error {
-// 	return nil
-// }
-//
-// func (m *MockSubscriber) Close() error {
-// 	return nil
-// }
-
-// TestSubscriberInterface ensures that our interface is correctly defined
 func TestSubscriberInterface(t *testing.T) {
 	ctx := context.Background()
 	Subscriber := &spy.Subscriber{}
