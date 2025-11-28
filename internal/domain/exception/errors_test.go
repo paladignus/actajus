18acfb2ab30f33e13ad8952570855858f1412c23
paladignus <marcelo@marcelo.eti.br>
2025-11-28 09:10:42 -0400

    test: ensure that errors are validi and of the same type

---
+++ internal/domain/exception/errors_test.go
@@ -0,0 +1,41 @@
+package exception
+
+import (
+	"testing"
+)
+
+func TestErrorVariables(t *testing.T) {
+	tests := []struct {
+		err      error
+		expected string
+	}{
+		{ErrInvalidFirstName, "first name is invalid"},
+		{ErrInvalidLastName, "last name is invalid"},
+		{ErrInvalidBirthDate, "birth date is invalid"},
+		{ErrInvalidMother, "mother is invalid"},
+		{ErrInvalidFather, "father is invalid"},
+		{ErrInvalidGender, "gender is invalid"},
+		{ErrInvalidMarital, "marital status is invalid"},
+		{ErrUserNotFound, "user not found"},
+		{ErrInvalidCredentials, "invalid credentials"},
+		{ErrInvalidToken, "invalid token"},
+		{ErrTokenExpired, "token expired"},
+		{ErrUnauthorized, "unauthorized"},
+		{ErrForbidden, "forbidden"},
+		{ErrInvalidUserData, "invalid user data"},
+		{ErrUserAlreadyExists, "user already exists"},
+		{ErrInvalidCPF, "invalid cpf"},
+		{ErrCPFNotFound, "user not found"},
+		{ErrEmailNotFound, "email not found"},
+		{ErrEmailAlreadyExists, "email already exists"},
+		{ErrEmailNotVerified, "email not verified"},
+		{ErrEmailAlreadyVerified, "email already verified"},
+		{ErrInvalidEmail, "invalid email"},
+		{ErrInvalidPassword, "invalid password"},
+	}
+	for _, test := range tests {
+		if test.err.Error() != test.expected {
+			t.Errorf("Expected error '%s', got '%s'", test.expected, test.err.Error())
+		}
+	}
+}
