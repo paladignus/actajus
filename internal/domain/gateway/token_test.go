4c5149659d79230ee32b426a8249bc4defcba4c1
paladignus <marcelo@marcelo.eti.br>
2025-11-28 10:26:11 -0400

    test: ensure that the token interface is valid

---
+++ internal/domain/gateway/token_test.go
@@ -0,0 +1,32 @@
+package gateway
+
+import (
+	"testing"
+
+	"github.com/paladignus/actajus/internal/application/dto"
+	"github.com/paladignus/actajus/test/spy"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTokenInterface(t *testing.T) {
+	sut := &spy.Token{}
+	tokenPair, err := sut.GenerateTokenPair("user-id")
+	assert.NoError(t, err)
+	resetToken, err := sut.GenerateResetToken("user-id")
+	assert.NoError(t, err)
+	tokenClaims, err := sut.ValidateAccessToken("access-token")
+	assert.NoError(t, err)
+	refreshClaims, err := sut.ValidateRefreshToken("refresh-token")
+	assert.NoError(t, err)
+	resetClaims, err := sut.ValidateResetToken("reset-token")
+	assert.NoError(t, err)
+	refreshedPair, err := sut.RefreshAccessToken("refresh-token")
+	assert.NoError(t, err)
+	assert.Equal(t, tokenPair, dto.TokenPair{})
+	assert.Equal(t, resetToken, dto.TokenRecover{})
+	assert.Equal(t, tokenClaims, dto.TokenClaims{})
+	assert.Equal(t, refreshClaims, dto.TokenClaims{})
+	assert.Equal(t, resetClaims, dto.TokenClaims{})
+	assert.Equal(t, refreshedPair, dto.TokenPair{})
+}
+
