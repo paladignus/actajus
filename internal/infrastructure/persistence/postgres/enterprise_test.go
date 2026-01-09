af11a1ede87b1c365139ab51d4623d39f96d827f
paladignus <marcelo@marcelo.eti.br>
2026-01-09 11:49:42 -0400

    test: ensure persitence to enterprise are valid

--- internal/infrastructure/persistence/postgres/enterprise_test.go
+++ internal/infrastructure/persistence/postgres/enterprise_test.go
@@ -6,28 +6,12 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/paladignus/actajus/internal/application/dto"
 	"github.com/paladignus/actajus/internal/domain/entity"
 	"github.com/paladignus/actajus/internal/infrastructure/database"
 	"github.com/pashagolub/pgxmock/v4"
 	"github.com/stretchr/testify/assert"
 )
-
-type Enterprise struct {
-	db *database.DB
-}
-
-func NewEnterprise(db *database.DB) *Enterprise {
-	return &Enterprise{db: db}
-}
-
-func (e Enterprise) Create(ctx context.Context, enterprise entity.Enterprise) error {
-	sql := `INSERT INTO enterprises (name, trade_name, cnpj) VALUES ($1, $2, $3);`
-	_, err := e.db.Pool.Exec(ctx, sql, enterprise.Name, enterprise.TradeName, enterprise.CNPJ)
-	if err != nil {
-		return fmt.Errorf("database error while saving enterprise: %w", err)
-	}
-	return nil
-}
 
 func TestEnterprise(t *testing.T) {
 	ctx := context.Background()
@@ -36,17 +20,13 @@ func TestEnterprise(t *testing.T) {
 	defer mock.Close()
 	db := &database.DB{Pool: mock}
 	repo := NewEnterprise(db)
-	enterprise, err := entity.NewEnterprise("Actajus", "Actajus Trade", "10.123.456/0001-00")
+	input := dto.EnterpriseInput{RegisteredBy: 1, Name: "Actajus", TradeName: "Actajus Trade", CNPJ: "10.123.456/0001-00"}
+	enterprise, err := entity.NewEnterprise(input)
 	assert.NoError(t, err)
-	// enterprise := entity.Enterprise{
-	// 	Name:      "actajus",
-	// 	TradeName: "actajus-trade",
-	// 	CNPJ:      "10.123.456/0001-00",
-	// }
 
 	t.Run("should be successfully inseted into the database", func(t *testing.T) {
 		mock.ExpectExec(`INSERT INTO enterprises`).
-			WithArgs(enterprise.Name, enterprise.TradeName, enterprise.CNPJ).
+			WithArgs(enterprise.RegisteredBy, enterprise.Name, enterprise.TradeName, enterprise.CNPJ).
 			WillReturnResult(pgxmock.NewResult("INSERT", 1))
 		err := repo.Create(ctx, enterprise)
 		assert.NoError(t, err)
@@ -56,7 +36,7 @@ func TestEnterprise(t *testing.T) {
 	t.Run("should return error when database fails to insert", func(t *testing.T) {
 		mockErr := fmt.Errorf("database error")
 		mock.ExpectExec(`INSERT INTO enterprises`).
-			WithArgs(enterprise.Name, enterprise.TradeName, enterprise.CNPJ).
+			WithArgs(enterprise.RegisteredBy, enterprise.Name, enterprise.TradeName, enterprise.CNPJ).
 			WillReturnError(mockErr)
 		err := repo.Create(ctx, enterprise)
 		assert.Error(t, err)
