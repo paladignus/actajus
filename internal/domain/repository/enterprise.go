5a41c13aaa2c7f227a6b8cb7ebf4415e11c93d15
paladignus <marcelo@marcelo.eti.br>
2026-01-09 11:47:11 -0400

    feat: add persistence repository to enterprise

---
+++ internal/domain/repository/enterprise.go
@@ -0,0 +1,12 @@
+// Package repository
+package repository
+
+import (
+	"context"
+
+	"github.com/paladignus/actajus/internal/domain/entity"
+)
+
+type IEnterprise interface {
+	Create(ctx context.Context, enterprise entity.Enterprise) error
+}
