ad5acd3a28868874f956fb4f2f62d0046504b32f
paladignus <marcelo@marcelo.eti.br>
2026-02-19 13:25:15 -0400

    refactor: removed method delete to repository

--- internal/module/company/domain/company_email_repository.go
+++ internal/module/company/domain/company_email_repository.go
@@ -5,6 +5,4 @@ import "context"
 
 type CompanyEmailRepository interface {
 	Create(ctx context.Context, idCompany, idEmail uint) error
-	DeleteByIDCompany(ctx context.Context, idCompany uint) error
-	// DeleteByIDAddress(ctx context.Context, idAddress uint) error
 }
